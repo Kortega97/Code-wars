7141ef9ab32b9ede3677dcdcc2b7af2b11cdd445
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-02-22 19:42:49 -0500

    Mon2 22 update

---
+++ Feb 22 - 28th /main.js
@@ -0,0 +1,24 @@
+// Mon-22
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Example:
+
+// makeNegative(1); // return -1
+// makeNegative(-5); // return -5
+// makeNegative(0); // return 0
+// makeNegative(0.12); // return -0.12
+// Notes:
+
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+function makeNegative(num) {
+    return -Math.abs(num) 
+      }
+//   refrences   
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
+
+// https://stackoverflow.com/questions/5574144/positive-number-to-negative-number-in-javascript
