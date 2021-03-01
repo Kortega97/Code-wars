b2a6722eb6ba9297b0f3ebfd626f895b35bcfbc7
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-03-01 13:47:36 -0500

    March 1st update

---
+++ March 1st - 7th/main.js
@@ -0,0 +1,11 @@
+// Monday March 1st 
+// Convert a Boolean to a String
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+ function booleanToString(b){
+ if ((b === true) || (b === false)){
+    return b.toString(b)
+  } //your code here
+ }
