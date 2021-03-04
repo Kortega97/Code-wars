acc335d81e82896c5c703b7460ebee5e17cba2ca
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-03-04 02:23:03 -0500

    march 3rd update

--- March 1st - 7th/main.js
+++ March 1st - 7th/main.js
@@ -17,3 +17,15 @@
 function filter_list(l) {
     return l.filter(num => typeof(num) === 'number')// Return a new array with the strings filtered out
    }
+
+  
+  //  March 3rd
+  
+  // Remove First and Last Character
+
+  // It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+  function removeChar(str) { 
+    str.slice(1); 
+     str.slice(0, str.length - 1); 
+     return str
+            } 
