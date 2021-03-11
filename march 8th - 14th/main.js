62df31a329ec9bf36b4768fe63581396938b7f57
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-03-10 21:29:14 -0500

    march 10th update

--- march 8th - 14th/main.js
+++ march 8th - 14th/main.js
@@ -2,3 +2,14 @@
 // Complete the solution so that it reverses the string passed into it.
 
 return str.split('').reverse().join('') 
+
+// march 10th
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+var stringToNumber = function(str){
+    // put your code here
+     return parseInt(str);
+   }
+
