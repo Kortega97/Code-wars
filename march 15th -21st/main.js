98ff92edfc085f28e7e58f260de513b15965154f
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-03-16 22:33:07 -0400

    march 16th update

---
+++ march 15th -21st/main.js
@@ -0,0 +1,22 @@
+// march 16th
+
+// Numbers to Letters
+
+// Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+
+// All inputs will be valid.
+
+function switcher(x){
+    let convertedArry = ""
+  x.forEach(letter =>{ let val = Number(letter);
+    switch(val){
+        case 27: convertedArry+='!'
+        break
+        case 28: convertedArry+='?'
+        break
+        case 29: convertedArry+=' '
+        break
+        default: convertedArry+= String.fromCharCode((val-27)*(-1) +96) ; console.log()
+    }
+   })
+  return convertedArry
