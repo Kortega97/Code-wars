64b5e6ee39589195fe6f4489cac63f44807080c5
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-03-17 14:09:14 -0400

    March 17th Update

--- march 15th -21st/main.js
+++ march 15th -21st/main.js
@@ -20,3 +20,16 @@ function switcher(x){
     }
    })
   return convertedArry
+
+// March 17th
+
+//   Convert boolean values to strings 'Yes' or 'No'.
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+    if(bool === true){
+      return "Yes"}
+    else{return 'No'}//...
+  }
+
