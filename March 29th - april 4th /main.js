765150b22cc90247d299de864f8716f6ca0e5616
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-03-31 08:47:23 -0400

    march 31st update

--- March 29th - april 4th /main.js
+++ March 29th - april 4th /main.js
@@ -33,3 +33,13 @@ var replaceDots = function(str) {
     
     }
 
+    // Super Duper Easy
+
+    // Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+    function problem(x){
+        if(x ===`${x}`){
+          return "Error"
+        }else{return (x * 50) + 6}
+        //your code here
+      }
