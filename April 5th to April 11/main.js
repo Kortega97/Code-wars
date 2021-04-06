8f642a5c7058c39dc29ba3154b32162a4a80f905
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-04-06 11:44:47 -0400

    A 6th update

--- April 5th to April 11/main.js
+++ April 5th to April 11/main.js
@@ -21,8 +21,24 @@ function maskify(cc){
   }
 
   
-// 
-// 
-// 
+// april 6th 
+// Get the mean of an array
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+function getAverage(marks){
+  var total = 0;
+ for(var i = 0; i < marks.length; i++) {
+     total += marks[i];
+ }
+ var avg = total / marks.length;
+  
+ 
+ return Math.floor(avg)
+   //TODO : calculate the downwar rounded average of the marks array
+ }
+ 
 // 
 // 
