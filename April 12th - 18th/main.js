74433cf9a49d154c090c2818513407e3632c2c18
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-04-13 11:32:22 -0400

    a 13th

--- April 12th - 18th/main.js
+++ April 12th - 18th/main.js
@@ -22,3 +22,19 @@ function isLeapYear(year) {
     
       // TODO
     }
+    // a 13th
+    // Kata Example Twist
+    // This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the array websites/Websites 1,000 times.
+
+var websites = [];
+while (websites.length < 1000) websites.push("codewars")
+
+
+
+
+    //  a14th
+    // a15th
+    // 16th
+    
