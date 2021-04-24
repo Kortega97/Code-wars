45f42b20b2e8d172c58c3c03a229bd2c0f7fe29e
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-04-23 23:46:12 -0400

    april 23rd commit

--- A 19th - 23/main.js
+++ A 19th - 23/main.js
@@ -52,9 +52,34 @@ function deleteValues(array, pred) {
 
 //  A 22
 
-*/
+
 
 function capitalize(s){
   return [s.split('').map((v,i)=>i%2==0?v.toUpperCase():v.toLowerCase()).join(''),
           s.split('').map((v,i)=>i%2!==0?v.toUpperCase():v.toLowerCase()).join('')]
 };
+
+// april 23
+
+// Description:
+// Given an array of positive integers, replace every element with the least greater element to its right. If there is no greater element to its right, replace it with -1. For instance, given the array
+// [8, 58, 71, 18, 31, 32, 63, 92, 43, 3, 91, 93, 25, 80, 28],
+// the desired output is
+// [18, 63, 80, 25, 32, 43, 80, 93, 80, 25, 93, -1, 28, -1, -1].
+// Your task is to create a function "arrayManip()" that takes in an array as its argument, manipulates the array as described above, then return the resulting array.
+// Note: Return a new array, rather than modifying the passed array.
+// */
+function arrayManip(array){
+  for (let i=0;i<array.length;i++){
+    let arr=[];
+      for (let j=i+1;j<array.length;j++){
+      if (array[i]<array[j]) arr.push(array[j]);  
+    }
+    let min=Math.min(...arr)
+    if (arr.length>0){
+    array[i]=min
+    } else {array[i]=-1};
+    arr=[];
+    }
+    return array
+}
