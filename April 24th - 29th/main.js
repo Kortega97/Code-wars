0df8640a207bd611ee4adfab55555807580177ba
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-04-26 23:56:31 -0400

    april 26TH commit

---
+++ April 24th - 29th/main.js
@@ -0,0 +1,21 @@
+// 24TH
+
+function cookingTime(eggs) {
+    return 5 * Math.ceil(eggs / 8);
+  }
+
+//   25TH
+
+function removeSmallest(numbers) {
+    let indexOfMin = numbers.indexOf(Math.min(...numbers));
+    return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
+  }
+// 26TH
+
+
+
+// 28TH
+
+
+
+// 29TH
