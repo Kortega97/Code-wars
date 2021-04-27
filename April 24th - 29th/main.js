120418e4f0f0c2d480796d120d7e4f9262881d33
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-04-27 00:06:38 -0400

    april 27TH commit

--- April 24th - 29th/main.js
+++ April 24th - 29th/main.js
@@ -7,27 +7,11 @@ function cookingTime(eggs) {
   }
 
 // 27TH
-
-/*
-Description:
-We have a broken message machine that introduces noise to our incoming messages. We know that our messages can't be written using %$&/#·@|º\ª and white characters (like spaces or tabs). Unfortunately our machine introduces noise, which means that our message arrives with characters like: %$&/#·@|º\ª within our original message.
-
-Your mission is to write a function which removes this noise from the message.
-
-Notice that noise can only be
-%$&/#·@|º\ª
-charaters, other characteres are not considered noise
-
-For example:
-
-removeNoise("h%e&·%$·llo w&%or&$l·$%d")
-// returns hello world
-*/
+function removeSmallest(numbers) {
+    let indexOfMin = numbers.indexOf(Math.min(...numbers));
+    return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
+  }
 
 
-function removeNoise(str){
-	return str.replace(/[%$&/#·@|º\\ª]/gi,'');
-}
-
-// 28TH
+// 29TH
 
