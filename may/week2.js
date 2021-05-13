3610732f2a6c73dc7eaa1fbb9382567df5365d98
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-05-13 08:21:06 -0400

    may 13th

--- may/week2.js
+++ may/week2.js
@@ -16,4 +16,5 @@ function hasUniqueChars(str) {  return new Set(str).size === str.length;}
 
 
 
-
+// 13th
+function caffeineBuzz(n) {  if (n % 12 === 0) return "CoffeeScript";  if (n % 6 === 0) return "JavaScript";  if (n % 3 === 0) return "Java";  return "mocha_missing!";}
