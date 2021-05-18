b15ef6248c1ff0a248b92dc930cb336afad3db28
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-05-18 08:28:01 -0400

    may 18th

--- may/week3.js
+++ may/week3.js
@@ -49,3 +49,17 @@ var romanToInt = function (s) {
     }
     return res;
 }
+
+
+// 18th
+
+var removeElement = function (nums, val) {
+    let curIndex = 0;
+    const arrLen = nums.length;
+    for (let i = 0; i < arrLen; i++) {
+        if (nums[i] !== val) {
+            nums[curIndex++] = nums[i];
+        }
+    }
+    return curIndex;
+};
