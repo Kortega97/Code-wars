de480ca255babada038b6321716c9f85c9e975b5
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-05-20 08:31:24 -0400

    may 19th

--- may/week3.js
+++ may/week3.js
@@ -75,16 +75,35 @@ var removeElement = function (nums, val) {
 //  * @param {number} target
 //  * @return {number[]}
 //  */
-let twoSum = function(nums, target) {
-	for (let i = nums.length - 1; i >= 0; i--) {
-		for (let j = 0; j < i; j++) {
-			if ( addition(nums[i], nums[j]) === target ) {
-				return [j, i];
-			}
-		}
-	}
+let twoSum = function (nums, target) {
+    for (let i = nums.length - 1; i >= 0; i--) {
+        for (let j = 0; j < i; j++) {
+            if (addition(nums[i], nums[j]) === target) {
+                return [j, i];
+            }
+        }
+    }
 };
 // add a with b and return it
-let addition = function(a, b) {
-	return a + b;
+let addition = function (a, b) {
+    return a + b;
+};
+
+// 20
+
+var reverse = function (x) {
+    let res = 0,
+        abs = Math.abs(x),
+        negative = abs === x ? false : true;
+    while (abs > 0) {
+        let currentVal = abs % 10;
+        res = res * 10 + currentVal;
+        abs = Number.parseInt(abs / 10);
+    }
+    if (res > Math.pow(2, 31)) {
+        return 0;
+    } else if (negative) {
+        return -res;
+    }
+    return res;
 };
