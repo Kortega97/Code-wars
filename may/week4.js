75d7d5b52733be187def4c3a21be13b3b8ee4cc1
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-05-24 12:31:58 -0400

    may 24th high and low 7th kyu

---
+++ may/week4.js
@@ -0,0 +1,12 @@
+// -24th
+function highAndLow(numbers){
+    let nums = numbers.split(" ");
+      return `${Math.max(...nums)} ${Math.min(...nums)}`;
+    }
+// -25th
+
+// -26th
+
+// -27th
+
+// -28th
