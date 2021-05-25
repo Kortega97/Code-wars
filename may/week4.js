694bb425e0cc2f14c5ccb70449fde35210d2087d
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-05-25 12:37:36 -0400

    may 25th insert dashes 7th kyu

--- may/week4.js
+++ may/week4.js
@@ -4,7 +4,9 @@ function highAndLow(numbers){
       return `${Math.max(...nums)} ${Math.min(...nums)}`;
     }
 // -25th
-
+function insertDash(num) {
+    return num.toString().replace(/[13579](?=[13579])/g, "$&-");
+ }
 // -26th
 
 // -27th
