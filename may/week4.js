df69565a1c4f68feae2809d8ef2473014dde9324
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-05-26 12:39:53 -0400

    may 26th insert dashes two 7th kyu

--- may/week4.js
+++ may/week4.js
@@ -8,7 +8,16 @@ function insertDash(num) {
     return num.toString().replace(/[13579](?=[13579])/g, "$&-");
  }
 // -26th
-
+function insertDashII(num) {
+    num=num.toString().split('')
+    const arr=[];
+    for (let i=0;i<num.length;i++)
+    {arr.push(num[i])
+    if (num[i]%2===0&&num[i+1]%2===0&&num[i]!=='0'&&num[i+1]!=='0'){arr.push('*')}
+    if (num[i]%2!==0&&num[i+1]%2!==0&&num[i+1]!==undefined){arr.push('-')}
+    }
+    return arr.join('')
+    }
 // -27th
 
 // -28th
