9e1b9a7722e7a2524373bf925eef973af1b7c222
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-06-04 12:34:28 -0400

    june 4th

--- june/week1.js
+++ june/week1.js
@@ -24,3 +24,11 @@ function rockBottom(arr) {
     return arr.replace(/ /g,'PLHHHH')
   }
 // 4th
+/*
+Description:
+Fix the code, so any written string put in function returns enclosed in two hearts. <3 Please give string a bit of space before and after a heart! <3
+*/
+function hearts(n) {
+    return `<3 ${n} <3`
+  }
+  
