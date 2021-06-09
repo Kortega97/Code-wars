6115b24270764567eaf6fc038df795c7448a234e
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-06-09 00:47:37 -0400

    jun9th

--- june/week2.js
+++ june/week2.js
@@ -29,7 +29,17 @@ const totalLicks = (env) => {
 // 8th
 String.prototype.repeat = String.prototype.repeat
 // 9th 
-
+function shadesOfGrey(n) {
+    var shades = []
+    
+    for (var i = 1; i <= Math.min(n, 254); i++) {
+      var grey = ('0' + i.toString(16)).slice(-2)
+      shades.push('#' + grey + grey + grey)
+    }
+    
+    return shades
+  }
+  
 // 10th
 
 // 11th
