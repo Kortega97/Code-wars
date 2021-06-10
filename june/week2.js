9d8d074f650b0f74a3caf1fe6fecb33494c702f5
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-06-10 00:48:40 -0400

    jun10th

--- june/week2.js
+++ june/week2.js
@@ -41,5 +41,13 @@ function shadesOfGrey(n) {
   }
   
 // 10th
-
+const seven = m => {
+    let times = 0
+    while ((''+m).length > 2) {
+      times++
+      m = Math.floor(m / 10) - (m % 10) * 2
+    }
+    return [m, times]
+  }
+  
 // 11th
