f791303f23bd6d9ae30c9d01eba45d7d8c1721eb
Raymond Ortega <kiany.s.ortega@gmail.com>
2021-06-16 11:13:43 -0400

    jun16th

--- june/week3.js
+++ june/week3.js
@@ -31,7 +31,18 @@ function search(budget, prices) {
   }
   
 // 16th
+/*
+Description:
+Is Undefined?
+Help Timmy remove undefined values from his array, you are modifing a callback function which he uses on each element on his array.
+Task
+Your task is to finish the isUndefined method which returns true if the value is undefined :)
+*/
 
+function isUndefined(value) {
+    return value===undefined
+  }
+  
 // 17th
 
 // 18th
