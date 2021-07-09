23a3631d4cf22770c71c907de2243a5850b5138d
Kiany Ortega <kiany.s.ortega@gmail.com>
2021-07-09 12:18:06 -0400

    july 9th

--- july/week2.js
+++ july/week2.js
@@ -156,3 +156,18 @@ class SolutionRecursive {
       }
   };
 // 9th
+class Solution {
+  public:
+      bool checkSymmetry(const TreeNode* treeL, const TreeNode* treeR) {
+          if (treeL == nullptr && treeR == nullptr) {
+              return true;
+          } else if (treeL != nullptr && treeR != nullptr) {
+              return treeL->val == treeR->val && checkSymmetry(treeL->left, treeR->right) && checkSymmetry(treeL->right, treeR->left);
+          }
+          return false;
+      }
+      
+      bool isSymmetric(TreeNode* root) {
+          return root == nullptr || checkSymmetry(root->left, root->right);
+      }
+  };
