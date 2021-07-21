eb292eb15b8d4e2622058c22c8f9374b92a30cc1
Kiany Ortega <kiany.s.ortega@gmail.com>
2021-07-21 12:34:42 -0400

    july 21st

--- july/week4.js
+++ july/week4.js
@@ -1,6 +1,4 @@
 // 19th
-// 53. Maximum Subarray
-// Easy   39%
 
 // Find the contiguous subarray within an array (containing at least one number)
 // which has the largest sum.
@@ -94,8 +92,6 @@
     return divideAndConquer(0, nums.length - 1)
   }
 // 20th
-// 1. Two Sum
-// Easy  35%
 
 // Given an array of integers, return indices of the two numbers such that they
 // add up to a specific target.
@@ -131,5 +127,58 @@
     console.log(twoSum(...args))
   })
 // 21st
+
+
+// Reverse digits of an integer.
+
+// Example1: x = 123, return 321
+// Example2: x = -123, return -321
+
+// click to show spoilers.
+//   Have you thought about this?
+
+// Here are some good questions to ask before coding. Bonus points for you if
+// you have already thought through this!
+
+// If the integer's last digit is 0, what should the output be? ie, cases such
+// as 10, 100.
+
+// Did you notice that the reversed integer might overflow? Assume the input is
+// a 32-bit integer, then the reverse of 1000000003 overflows. How should you
+// handle such cases?
+
+// For the purpose of this problem, assume that your function returns 0 when the
+// reversed integer overflows.
+
+// Note: The input is assumed to be a 32-bit signed integer. Your function
+// should return 0 when the reversed integer overflows.
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+
+ const reverse = function(x) {
+    const isOverflow = x => x > 0x7fffffff || -x > 0x7fffffff
+    if (isOverflow(x)) return 0
+  
+    let y = Math.abs(x), result = 0
+    while (y !== 0) {
+      result = result * 10 + y % 10
+      y = Math.floor(y / 10)
+    }
+  
+    result *= x < 0 ? -1 : 1
+    return isOverflow(result) ? 0 : result
+  }
+  
+  ;[
+    123,                          // 321
+    -123,                         // -321
+    -2147483648,                  // 0
+    1534236469,                   // 0
+  ].forEach(x => {
+    console.log(reverse(x))
+  })
 // 22nd 
 // 23rd
