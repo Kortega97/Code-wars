5829a3fdef8a45cf566d716bc96cc01a5083e338
Kiany Ortega <kiany.s.ortega@gmail.com>
2021-07-23 12:37:02 -0400

    july 23rd

--- july/week4.js
+++ july/week4.js
@@ -217,3 +217,38 @@
     console.log(isPalindrome(x))
   })
 // 23rd
+// Given a roman numeral, convert it to an integer.
+
+// Input is guaranteed to be within the range from 1 to 3999.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ const romanToInt = function(s) {
+    const Roman = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+    }
+  
+    let result = 0
+    for (let i = s.length - 1; i >= 0; i--) {
+      const high = Roman[s[i]], low = (Roman[s[i + 1]] || 0)
+      result += (high < low ? -1 : 1) * high
+    }
+  
+    return result
+  }
+  
+  ;[
+    'I',                          // 1
+    'IV',                         // 4
+    'MMMCMXCIX',                  // 3999
+  ].forEach(s => {
+    console.log(romanToInt(s))
+  })
